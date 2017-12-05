a0ff027890fcb2ae5797edc916ba9799685980ba
davinascott <32618166+davinascott@users.noreply.github.com>
2017-12-05 04:01:52 -0500

    updated schema

--- comp2180-project3a/workspace/workspace/schema.sql
+++ comp2180-project3a/workspace/workspace/schema.sql
@@ -3,16 +3,16 @@ DROP TABLE IF EXISTS MessagesRead;
 DROP TABLE IF EXISTS Users;
 
 CREATE TABLE Users(
-id INT UNSIGNED NOT NULL PRIMARY KEY,
+id int AUTO_INCREMENT NOT NULL PRIMARY KEY,
 firstname varchar(30) NOT NULL,
 lastname varchar(30) NOT NULL,
 username varchar(30) NOT NULL,
 password varchar(60) NOT NULL,
-session_id varchar(60) NOT NULL
+mysession int(20) NOT NULL
 );
 
 CREATE TABLE Messages(
-id INT UNSIGNED NOT NULL PRIMARY KEY,
+id int AUTO_INCREMENT NOT NULL PRIMARY KEY,
 recepient_ids varchar(120) NOT NULL,
 sender_id varchar(50) NOT NULL,
 subject varchar(50) NOT NULL,
@@ -21,8 +21,8 @@ date_sent TIMESTAMP NOT NULL
 );
 
 CREATE TABLE MessagesRead(
-id INT UNSIGNED NOT NULL PRIMARY KEY,
-message_id int(11) NOT NULL,
+id int AUTO_INCREMENT NOT NULL PRIMARY KEY,
+message_id int(25) NOT NULL,
 reader_id varchar(50) NOT NULL,
 date_read TIMESTAMP NOT NULL
 );
